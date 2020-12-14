478b081c0844dbb0e24d61964932ac4fa10721f9
Di Zou <dzou@marchnetworks.com>
2020-12-14 00:21:26 -0500

    small fixes for #2

--- example/demo.sh
+++ example/demo.sh
@@ -6,16 +6,16 @@ vagrant up
 vagrant ssh k3os-server -c 'sudo /home/rancher/scripts/configure_k3s_server.sh not4you2see! 192.168.33.10'
 vagrant ssh k3os-1 -c 'sudo /home/rancher/scripts/configure_k3s_node.sh not4you2see! 192.168.33.10 192.168.33.11'
 
-# echo "get kube config ..."
+read -p "you need to get kube.config manually, check read me, press any key to continue"
 # ./update-kube-config.sh
 
-# echo "==== Show k8s nodes ===="
-# KUBECONFIG=kube.config kubectl get nodes
+echo "==== Show k8s nodes ===="
+KUBECONFIG=kube.config kubectl get nodes
 
-# echo "==== Deploy Hello-world ===="
-# KUBECONFIG=kube.config kubectl apply -f example/hello-world.yaml
-# KUBECONFIG=kube.config kubectl expose deployment hello-world --type=NodePort --name=example-service
+echo "==== Deploy Hello-world ===="
+KUBECONFIG=kube.config kubectl apply -f example/hello-world.yaml
+KUBECONFIG=kube.config kubectl expose deployment hello-world --type=NodePort --name=example-service
 
-# port=$(KUBECONFIG=kube.config kubectl describe services example-service | grep "NodePort")
+port=$(KUBECONFIG=kube.config kubectl describe services example-service | grep "NodePort")
 
-# read -p "go to http://192.168.33.11:${port}"
+read -p "go to http://192.168.33.11:${port}"
