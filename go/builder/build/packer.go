a381a9919561c3810037fd280ae0c3941ada55fd
zodiac1214 <di.zou1214@gmail.com>
2020-12-22 22:02:55 -0500

    ground work for packer

--- go/builder/build/packer.go
+++ go/builder/build/packer.go
@@ -1,8 +1,11 @@
 package build
 
 import (
+	"bufio"
 	"context"
+	"embed"
 	"fmt"
+	"log"
 )
 
 /*
@@ -11,4 +14,21 @@ interact with hashicorp's packer lib to create machine images
 
 func Packer(ctx context.Context, param BuildParameters) {
 	fmt.Println("build packer")
+
+	//todo: this just shows how to pack static files in go binary
+
+	file, _ := PackerFiles.Open("packer/system-images.list")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		image := scanner.Text()
+		out, err := pullAndSave(image)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(out)
+	}
+
 }
+
+//go:embed packer/*
+var PackerFiles embed.FS
