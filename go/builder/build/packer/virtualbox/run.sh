4463d0f27a9c3765f943cdc6a5fba7f68458deb2
zodiac1214 <di.zou1214@gmail.com>
2020-12-30 00:36:24 -0500

    change run script to use native vbox command, TODO: keep fixing it

--- go/builder/build/packer/virtualbox/run.sh
+++ go/builder/build/packer/virtualbox/run.sh
@@ -1,22 +1,36 @@
 #!/bin/sh
-#TODO: remove vagrant dep
-read -p "turn off wifi!!!! (press any key to continue ...)"
-vagrant destroy -f
-vagrant box remove k3os --force
-vagrant box add ../../k3os_virtualbox.box --name k3os --force
 
-vagrant plugin install vagrant-disksize
-vagrant up
+createVM () {
+  VM_NAME="$1"
+  VBoxManage controlvm $VM_NAME acpipowerbutton || true
+  VBoxManage unregistervm --delete $VM_NAME || true
+  VBoxManage import output-virtualbox/packer-virtualbox-*.ovf  \
+    --vsys 0 \
+    --ostype "Linux_64" \
+    --vmname $VM_NAME \
+    --group "/" \
+    --cpus 2 \
+    --memory 2048
+  VBoxManage modifyvm $VM_NAME --nic1 hostonly --hostonlyadapter1 vboxnet0
+  VBoxManage startvm $VM_NAME --type headless
+  macAddress=`vboxmanage showvminfo $VM_NAME | grep "NIC 1" | awk -F'MAC: ' '{print $2}' | awk -F',' '{print $1}'`
+  sleep 60
+  ip=`vboxmanage dhcpserver findlease --interface vboxnet0 --mac-address=$macAddress | grep "IP" | awk -F'Address:  ' '{print $2}'`
+  chmod 400 ssh-default.pem
+  ssh -o StrictHostKeyChecking=no -i ./ssh-default.pem rancher@$ServerIp "$2"
+}
+createVM "server" "ls"
 
-vagrant ssh k3os-server -c 'sudo bash /home/rancher/scripts/configure_k3s_server.sh not4you2see! 192.168.33.10'
-vagrant ssh k3os-1 -c 'sudo bash /home/rancher/scripts/configure_k3s_node.sh not4you2see! 192.168.33.10 192.168.33.11'
 
-sleep 60
-
-echo "==== Get Kube Config ===="
-bash ./update-kube-config.sh
-
-echo "==== Show k8s nodes ===="
-KUBECONFIG=kube.config kubectl get nodes
-
-cd $pwd
+#vagrant ssh k3os-server -c 'sudo bash /home/rancher/scripts/configure_k3s_server.sh not4you2see! 192.168.33.10'
+#vagrant ssh k3os-1 -c 'sudo bash /home/rancher/scripts/configure_k3s_node.sh not4you2see! 192.168.33.10 192.168.33.11'
+#
+#sleep 60
+#
+#echo "==== Get Kube Config ===="
+#bash ./update-kube-config.sh
+#
+#echo "==== Show k8s nodes ===="
+#KUBECONFIG=kube.config kubectl get nodes
+#
+#cd $pwd
