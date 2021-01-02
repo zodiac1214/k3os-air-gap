a8be43f2ff4dac42b7de9fedaab0af8ac34e3370
zodiac1214 <di.zou1214@gmail.com>
2021-01-01 20:26:59 -0500

    fix missing images

--- go/builder/build/packer/virtualbox/run.sh
+++ go/builder/build/packer/virtualbox/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+set -e
 createVM () {
   VM_NAME="$1"
   VBoxManage controlvm $VM_NAME acpipowerbutton || true
@@ -13,24 +13,25 @@ createVM () {
     --memory 2048
   VBoxManage modifyvm $VM_NAME --nic1 hostonly --hostonlyadapter1 vboxnet0
   VBoxManage startvm $VM_NAME --type headless
+}
+
+findIP() {
+  VM_NAME="$1"
   macAddress=`vboxmanage showvminfo $VM_NAME | grep "NIC 1" | awk -F'MAC: ' '{print $2}' | awk -F',' '{print $1}'`
   sleep 60
   ip=`vboxmanage dhcpserver findlease --interface vboxnet0 --mac-address=$macAddress | grep "IP" | awk -F'Address:  ' '{print $2}'`
-  chmod 400 ssh-default.pem
-  ssh -o StrictHostKeyChecking=no -i ./ssh-default.pem rancher@$ServerIp "$2"
+  echo $ip
 }
-createVM "server" "ls"
+chmod 400 ssh-default.pem
 
+createVM "server"
+findIP "server"
+ssh -o StrictHostKeyChecking=no -i ./ssh-default.pem rancher@$ip "sudo bash scripts/configure_k3s_server.sh thisistoken $ip"
+ServerIP=$ip
 
-#vagrant ssh k3os-server -c 'sudo bash /home/rancher/scripts/configure_k3s_server.sh not4you2see! 192.168.33.10'
-#vagrant ssh k3os-1 -c 'sudo bash /home/rancher/scripts/configure_k3s_node.sh not4you2see! 192.168.33.10 192.168.33.11'
-#
-#sleep 60
-#
-#echo "==== Get Kube Config ===="
-#bash ./update-kube-config.sh
-#
-#echo "==== Show k8s nodes ===="
-#KUBECONFIG=kube.config kubectl get nodes
-#
-#cd $pwd
+createVM "node"
+findIP "node"
+ssh -o StrictHostKeyChecking=no -i ./ssh-default.pem rancher@$ip "sudo bash scripts/configure_k3s_node.sh thisistoken $ServerIP $ip"
+
+# TODO: install rancher
+# helm install rancher rancher-stable/rancher --version 2.5.3 --set tls=external
